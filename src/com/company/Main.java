56f7b82c011fa9d2390dbc1f9d8e4921de114373
gramsey1 <graceramsey312@yahoo.com>
2018-11-09 11:22:34 -0500

    ya

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,19 +5,27 @@ import java.io.File;
 public class Main {
 
     public static void main(String[] args) throws IOException{
-//0, 1 = School name, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18 = math average, 19 = reading average, 20 = writing average, 21 = percent tested
         Scanner sf = new Scanner (new File ("scores.txt"));
-
         String array [] = new String [1000];
 
-        int maxIndx = -1;
-        while(sf.hasNext()) {
-            maxIndx++;
-            array[maxIndx]= sf.nextLine();
+        int maxIndx = 0;
+        while(sf.hasNextLine()) {
+
+            String s = sf.nextLine();
+            String dumbarray [] = s.split("\\t");
+            if(dumbarray.length == 22) {
+
+            }
+            
+//
         }
-        System.out.println(array[1]);
+
+        System.out.print(array[1]);
         String newArray [] = new String [10000];
 
-        for ()
+
+
+
+
     }
 }
